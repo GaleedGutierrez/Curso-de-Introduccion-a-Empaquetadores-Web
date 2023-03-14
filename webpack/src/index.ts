3efba8d75d4c5c82f91e14f2f18d0ec01a965c25
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-03-14 10:34:33 -0300

    :white_check_mark: Clase N°7: Proyecto a empaquetar: Tu propio árbol de links

---
+++ webpack/src/index.ts
@@ -0,0 +1,38 @@
+const LINKS = document.getElementById('links');
+const NAME = document.querySelector('h1');
+
+const DATA = {
+	name: "Oscar Barajas",
+	nickname: "gndx",
+	description: "...",
+	avatar: "...",
+	social: [
+	{
+		name: "twitter",
+		url: "https://twitter.com/",
+		username: "",
+	},
+	{
+		name: "instagram",
+		url: "https://instagram.com/",
+		username: "",
+	},
+	],
+	links: [
+	{
+		name: "blog",
+		url: "https://example.com/",
+		color: "red",
+		emoji: "📖",
+	},
+	{
+		name: "podcast",
+		url: "https://example.com/",
+		color: "yellow",
+		emoji: "💬",
+	},
+	],
+	footer: "Made with Love on Colombia",
+};
+
+
--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-const PugPlugin = require('pug-plugin');
 
 module.exports = {
 	entry: './src/index.ts',
@@ -18,14 +17,6 @@ module.exports = {
 				use: 'babel-loader',
 				exclude: /node_modules/,
 		},
-		{
-			test: /\.pug$/,
-			loader: PugPlugin.loader, // Pug loader
-		},
-		{
-			test: /\.(css|sass|scss)$/,
-			use: ['css-loader', 'sass-loader'],
-		},
 		// {
 		// 	test: /\.js?$/,
 		// 	exclude: /node_modules/,
@@ -41,16 +32,5 @@ module.exports = {
 			template: './public/index.html',
 			filename: './index.html'
 		}),
-		new PugPlugin({
-			pretty: true, // formatting HTML, useful for development mode
-			js: {
-			  // output filename of extracted JS file from source script
-			filename: 'assets/js/[name].[contenthash:8].js',
-			},
-			css: {
-			  // output filename of extracted CSS file from source style
-			filename: 'assets/css/[name].[contenthash:8].css',
-			},
-		})
 	]
 }
